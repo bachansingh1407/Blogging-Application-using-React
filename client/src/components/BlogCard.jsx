7ee49ff37a2dcf39dbc57b0e95990a17f3960b9c
bachan singh <140685236+bachansingh1407@users.noreply.github.com>
2025-05-28 23:09:27 +0530

    Update BlogCard.jsx

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -1,1 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './blogcard.css'
+import moment from 'moment';
+
+const BlogCard = ({ blog }) => {
+  return (
+    <div className='blog__content'>
+
+      <div className="cover__photo">
+        {blog.image && (
+          <img
+            src={`http://localhost:5000/${blog.image}`}
+            alt={blog.title}
+          />
+        )}
+      </div>
+
+      <div className="text__content">
+
+        <p className='category'>{blog.category}</p>
+
+        {/* Blog Title with Link */}
+        <Link to={`/blog/${blog.id}`} ><h2>{blog.title}</h2></Link>
+
+        {/* Blog Content Preview */}
+        <p className='details'>{blog.content.substring(0, 150)}...</p>
+        <p className='posted__on'>Posted {moment(blog.created_at).fromNow()}</p>
+
+        {/* Author Link */}
+        <div className="profile__link">Author <Link to={`/dashboard/${blog.author_id}`}>
+          {blog.username}
+        </Link>
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default BlogCard;
 
